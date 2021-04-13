fad3828879374e4a5a20317306668d43a161b147
Rocky Warren <1085683+therockstorm@users.noreply.github.com>
2021-04-13 10:18:00 -0600

    refactor

--- src/pages/api/files.ts
+++ src/pages/api/files.ts
@@ -1,28 +1,28 @@
 import { FileList, VertexClient } from '@vertexvis/vertex-api-client';
 import type { NextApiRequest, NextApiResponse } from 'next';
+import { Env } from '../../lib/env';
 
 export default async function getFiles(
   _req: NextApiRequest,
   res: NextApiResponse<FileList>
 ): Promise<void> {
-  const c = await getClient();
-  res.status(200).json((await c.files.getFiles({ pageSize: 5 })).data);
+  const client = await getClient();
+  res.status(200).json((await client.files.getFiles({ pageSize: 5 })).data);
 }
 
-let client: VertexClient | undefined;
+let vertexClient: VertexClient | undefined;
 async function getClient(): Promise<VertexClient> {
-  if (client != null) return client;
+  if (vertexClient != null) return vertexClient;
 
-  const env = process.env.NEXT_PUBLIC_VERTEX_ENV;
-  client = await VertexClient.build({
+  vertexClient = await VertexClient.build({
     basePath:
-      env === 'platprod' || env === ''
+      Env === 'platprod'
         ? 'https://platform.vertexvis.com'
-        : `https://platform.${env}.vertexvis.io`,
+        : `https://platform.${Env}.vertexvis.io`,
     client: {
       id: process.env.VERTEX_CLIENT_ID ?? '',
       secret: process.env.VERTEX_CLIENT_SECRET ?? '',
     },
   });
-  return client;
+  return vertexClient;
 }
