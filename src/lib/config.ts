826d7250a485d0d3f4f107231d51acca7463c877
Rocky Warren <1085683+therockstorm@users.noreply.github.com>
2021-08-25 08:41:20 -0600

    Update default stream key

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -16,7 +16,7 @@ export const Config: Configuration = {
 // Vertex Valve
 export const DefaultCredentials: StreamCredentials = {
   clientId: "08F675C4AACE8C0214362DB5EFD4FACAFA556D463ECA00877CB225157EF58BFA",
-  streamKey: "U9cSWVb7fvS9k-NQcT28uZG6wtm6xmiG0ctU",
+  streamKey: "Eh96kzXEppNfcxj5gbbqdJ9oUdQPB7hXzHrU",
 };
 
 export function head<T>(items?: T | T[]): T | undefined {
