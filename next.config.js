de03d0a68cc0f9f63fe5d54edcc3638ba558cc15
Aaron Eischeid <a.eischeid@gmail.com>
2026-04-16 11:57:58 -0500

    more minor version bumps

--- next.config.js
+++ next.config.js
@@ -1,8 +1,4 @@
 module.exports = {
-  // Disable minifying with SWC until
-  // https://github.com/vercel/next.js/discussions/30237#discussioncomment-2950246
-  // is resolved.
-  swcMinify: false,
   webpack: (config) => {
     config.output.assetModuleFilename = `static/[hash][ext]`;
     config.output.publicPath = `/_next/`;
